f7e592edceac38e62f66344efb7ee0fbb0b8c9c4
vlodo-o <vabramochkina@yandex.ru>
2023-05-09 17:27:45 +0300

    mvc

--- app/src/main/java/com/practicum/moviesearch/Creator.kt
+++ app/src/main/java/com/practicum/moviesearch/Creator.kt
@@ -1,10 +1,13 @@
 package com.practicum.moviesearch
 
+import android.app.Activity
 import com.practicum.moviesearch.data.MoviesRepositoryImpl
 import com.practicum.moviesearch.data.network.RetrofitNetworkClient
 import com.practicum.moviesearch.domain.api.MoviesInteractor
 import com.practicum.moviesearch.domain.api.MoviesRepository
 import com.practicum.moviesearch.domain.impl.MoviesInteractorImpl
+import com.practicum.moviesearch.presentation.MoviesSearchController
+import com.practicum.moviesearch.ui.movies.MoviesAdapter
 
 object Creator {
     private fun getMoviesRepository(): MoviesRepository {
@@ -13,5 +16,9 @@ object Creator {
 
     fun provideMoviesInteractor(): MoviesInteractor {
         return MoviesInteractorImpl(getMoviesRepository())
+    }
+
+    fun provideMoviesSearchController(activity: Activity, adapter: MoviesAdapter): MoviesSearchController {
+        return MoviesSearchController(activity, adapter)
     }
 }
--- app/src/main/java/com/practicum/moviesearch/ui/movies/MoviesActivity.kt
+++ app/src/main/java/com/practicum/moviesearch/ui/movies/MoviesActivity.kt
@@ -5,39 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
-import android.widget.*
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.practicum.moviesearch.*
-import com.practicum.moviesearch.data.dto.MoviesSearchResponse
-import com.practicum.moviesearch.data.network.IMDbApiService
-import com.practicum.moviesearch.domain.api.MoviesInteractor
-import com.practicum.moviesearch.domain.models.Movie
 import com.practicum.moviesearch.ui.poster.PosterActivity
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MoviesActivity : Activity() {
 
-    private val moviesInteractor = Creator.provideMoviesInteractor()
-
     companion object {
         private const val CLICK_DEBOUNCE_DELAY = 1000L
-        private const val SEARCH_DEBOUNCE_DELAY = 2000L
     }
-
-    private lateinit var queryInput: EditText
-    private lateinit var placeholderMessage: TextView
-    private lateinit var moviesList: RecyclerView
-    private lateinit var progressBar: ProgressBar
-
-    private val movies = ArrayList<Movie>()
 
     private val adapter = MoviesAdapter {
         if (clickDebounce()) {
@@ -51,85 +26,17 @@ class MoviesActivity : Activity() {
 
     private val handler = Handler(Looper.getMainLooper())
 
-    private val searchRunnable = Runnable { searchRequest() }
-
+    private val moviesSearchController = Creator.provideMoviesSearchController(this, adapter)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movies)
-
-        placeholderMessage = findViewById(R.id.placeholderMessage)
-        queryInput = findViewById(R.id.queryInput)
-        moviesList = findViewById(R.id.locations)
-        progressBar = findViewById(R.id.progressBar)
-
-        adapter.movies = movies
-
-        moviesList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        moviesList.adapter = adapter
-
-        queryInput.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                searchDebounce()
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-            }
-
-        })
+        moviesSearchController.onCreate()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        handler.removeCallbacks(searchRunnable)
-    }
-
-    private fun searchRequest() {
-        if (queryInput.text.isNotEmpty()) {
-
-            placeholderMessage.visibility = View.GONE
-            moviesList.visibility = View.GONE
-            progressBar.visibility = View.VISIBLE
-
-            moviesInteractor.searchMovies(queryInput.text.toString(), object : MoviesInteractor.MoviesConsumer {
-                override fun consume(foundMovies: List<Movie>) {
-                    handler.post {
-                        progressBar.visibility = View.GONE
-                        movies.clear()
-                        movies.addAll(foundMovies)
-                        moviesList.visibility = View.VISIBLE
-                        adapter.notifyDataSetChanged()
-                        if (movies.isEmpty()) {
-                            showMessage(getString(R.string.nothing_found), "")
-                        } else {
-                            hideMessage()
-                        }
-                    }
-                }
-            })
-        }
-    }
-
-    private fun showMessage(text: String, additionalMessage: String) {
-        if (text.isNotEmpty()) {
-            placeholderMessage.visibility = View.VISIBLE
-            movies.clear()
-            adapter.notifyDataSetChanged()
-            placeholderMessage.text = text
-            if (additionalMessage.isNotEmpty()) {
-                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
-                    .show()
-            }
-        } else {
-            placeholderMessage.visibility = View.GONE
-        }
-    }
-
-    private fun hideMessage() {
-        placeholderMessage.visibility = View.GONE
+        moviesSearchController.onDestroy()
     }
 
     private fun clickDebounce() : Boolean {
@@ -139,10 +46,5 @@ class MoviesActivity : Activity() {
             handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
         }
         return current
-    }
-
-    private fun searchDebounce() {
-        handler.removeCallbacks(searchRunnable)
-        handler.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY)
     }
 }
